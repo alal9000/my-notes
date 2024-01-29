e4a90effab0c11547feffb219fd0e0de6d740a19
Aaron Lal <lucidlear@gmail.com>
2024-01-30 08:12:10 +1100

    before starting 30/1/2024

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import './App.css';
-import Header from './components/Header'
-import NotesListPage from './pages/NotesListPage'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Header from "./components/Header";
+import NotesListPage from "./pages/NotesListPage";
+import NotePage from "./pages/NotePage";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <NotesListPage />
-    </div>
+    <BrowserRouter>
+      <div className="container dark">
+        <div className="app">
+          <Header />
+          <Routes>
+            <Route path="/" element={<NotesListPage />} />
+            <Route path="/note/:id" element={<NotePage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
