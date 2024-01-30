27b56b05aa227571041bd32a67744573635894ce
Aaron Lal <lucidlear@gmail.com>
2024-01-30 12:42:21 +1100

    added fetch

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,26 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import notes from "../assets/data";
+// import notes from "../assets/data";
 import { Link } from "react-router-dom";
-import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
+import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
 
 const NotePage = (props) => {
   let { id } = useParams();
 
-  let note = notes.find((note) => note.id === Number(id));
+  // let note = notes.find((note) => note.id === Number(id));
+
+
+  let getNotes = async () => {
+    let response = await fetch("http://localhost:8000/notes");
+    let data = await response.json();
+    console.log("data:", data);
+  };
 
   return (
     <div className="note">
       <div className="note-header">
         <h3>
           <Link to="/">
-              <ArrowLeft />
+            <ArrowLeft />
           </Link>
         </h3>
       </div>
-      <textarea value={note?.body}>
-
-      </textarea>
+      {/* <textarea value={note?.body}></textarea> */}
     </div>
   );
 };
